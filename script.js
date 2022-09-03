e46ff32ba429c4c3e09d34b43237ba3023db572f
Edilson Quirino Do Nascimento Júnior <95941206+edilsonjrcode@users.noreply.github.com>
2022-09-03 16:44:51 -0300

    responsivo

---
+++ script.js
@@ -0,0 +1,8 @@
+function mudaPlano(id, titulo, paragrafo){
+    document.getElementById(id).style.background = '#a948a7';
+    document.getElementById(titulo).style.color = '#fff'
+    document.getElementById(paragrafo).style.color = '#fff'
+}
+
+
+
